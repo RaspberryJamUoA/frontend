c6d8ab98a9737f5fb8bdcba639b054074020463b
Sean Z <contact@seanzeng.com>
2020-08-09 11:56:24 +1200

    event detail page

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -7,13 +7,22 @@ export const mainTheme = createMuiTheme({
             primary: colors.blue,
             secondary: colors.deepPurple,
             background: {
-                default: colors.grey[100],
-                paper: colors.grey[200]
+                default: colors.grey[200],
+                paper: colors.grey[50]
             },
             text: {
                 primary: colors.common.black,
                 secondary: colors.blueGrey['50']
+            }
+        },
+        typography: {
+            h6: {
+                fontWeight: 300
             },
+            body2: {
+                fontSize: 20,
+                fontWeight: 300
+            }
         }
     }
 );
