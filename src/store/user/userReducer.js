949c44f284a336fdcb8f55745ce76a131557ca8c
Sean Z <contact@seanzeng.com>
2020-08-08 22:56:50 +1200

    add auth0 implementation

--- src/store/user/userReducer.js
+++ src/store/user/userReducer.js
@@ -1,16 +1,9 @@
-import {LOGIN_SUCCESS} from "./userActions";
-
 const initialState = {
-    loggedIn: false,
-    
 }
 
 const userReducer = (state = initialState, action) => {
 
     switch (action.type) {
-        case LOGIN_SUCCESS:
-            state.loggedIn = true;
-            return state;
         default:
             return state;
     }
