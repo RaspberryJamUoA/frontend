5d221d788ff5e0d23f7637f424607bdb1c64259a
Sean Z <contact@seanzeng.com>
2020-08-08 22:13:17 +1200

    fix event listing not updating

--- src/store/events/eventReducer.js
+++ src/store/events/eventReducer.js
@@ -8,9 +8,7 @@ const eventReducer = (state = initialState, action) => {
 
     switch (action.type) {
         case SET_EVENTS:
-            console.log(action.payload)
-            state.events = action.payload;
-            break;
+            return {...state, events: action.payload}
         case ADD_EVENT:
             const eventsCopy = [...state.events];
             eventsCopy.push(action.payload);
