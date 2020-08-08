f1c49cac4b54196025a01bd0f1cfd48e4025a855
Sean Z <contact@seanzeng.com>
2020-08-08 17:51:55 +1200

    theme provider

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import {
     makeStyles,
-    CssBaseline
+    CssBaseline,
+    ThemeProvider
 } from "@material-ui/core";
 import {Provider} from 'react-redux';
 import clsx from 'clsx';
