908ea2c2fc4161560a2695ae3162448df3795005
Raymond Feng <rf.raymondfeng@gmail.com>
2020-08-09 14:21:50 +1200

    [WPF-003] Fixed delay bug

--- src/store/events/eventReducer.js
+++ src/store/events/eventReducer.js
@@ -9,7 +9,7 @@ const eventReducer = (state = initialState, action) => {
 
     switch (action.type) {
         case SET_EVENTS:
-            return {...state, events: action.payload}
+            return {...state, events: [...action.payload]}
         case SET_INITIAL:
             return {...state, initialEvents: action.payload}
         case ADD_EVENT:
