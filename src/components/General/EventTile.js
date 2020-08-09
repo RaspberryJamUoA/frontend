5c39923898070470ee6a5dc9eec3a930c1058bcf
Daniel Li <danielli12357@gmail.com>
2020-08-09 15:06:20 +1200

    Add image link

--- src/components/General/EventTile.js
+++ src/components/General/EventTile.js
@@ -32,23 +32,23 @@ class EventTile extends Component {
     }
 
     render() {
+        console.log(this.props.imgLink);
         const dateString = this.formatDate();
-
         return (
             <div className="body">
                 <div className="events">
                     <div className='cardContent'>
                         <h2 className="title"><strong>{this.props.eventName}</strong></h2>
-                        <h5 className="date"><i className='far fa-calendar-alt'></i>  {dateString}</h5>
+                        <h5 className="date"><i className='far fa-calendar-alt'></i>  {dateString} </h5>
+                        <h5> <i className='fas fa-dollar-sign'></i> {(this.props.cost > 0) ?  this.props.cost : 'Free' }</h5>
                         <p className="description">{(this.props.description.length > 320) ? this.props.description.substring(0,320) + "\u2022 \u2022 \u2022" :this.props.description}</p>
-                        <p> <i className='fas fa-dollar-sign'></i> {(this.props.cost > 0) ?  this.props.cost : 'Free' }</p>
                     </div>
 
                     <div className="imgContainer">
                         <div className="heartOverlay" onClick={() => this.handleLikeClicked()}> 
                             <img id="heart" className="loveheart" src={this.heartSrc()} alt="heartImg" width="50px" height="50px"></img>
                         </div>
-                        <img className="eventImage" src="https://i.imgur.com/kuHv2UZ.png" alt="eventImg" width="400px" height="300px" ></img>
+                        <img className="eventImage" src={this.props.imgLink} alt="eventImg" width="400px" height="300px" ></img>
                     </div>
                 </div>
             </div>
