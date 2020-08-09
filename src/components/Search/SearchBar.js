b86dd6d99ec959df751c318e95138df66d13f559
Sean Z <contact@seanzeng.com>
2020-08-09 15:57:39 +1200

    create event

--- src/components/Search/SearchBar.js
+++ src/components/Search/SearchBar.js
@@ -1,14 +1,14 @@
 import React, {useState} from 'react';
 
-import {Paper, TextField} from '@material-ui/core';
+import {Paper, TextField, Grid, Button} from '@material-ui/core';
 import {makeStyles} from "@material-ui/core/styles";
 import {setEvents} from "../../store/events/eventActions";
 import {useDispatch, useSelector} from "react-redux";
+import {NewEventDialog} from "../General/NewEvent";
 
 const useStyles = makeStyles((theme) => ({
     search: {
-        margin: '0 30%',
-        width: "40%",
+
     },
     paper: {
         padding: theme.spacing(1)
@@ -21,6 +21,7 @@ export const SearchBar = () => {
     const dispatch = useDispatch();
     const classes = useStyles();
 
+    const [isOpen, setOpen] = useState(false);
     const [timer, setTimer] = useState(null);
 
 
@@ -32,7 +33,8 @@ export const SearchBar = () => {
 
             if ((event.clubName).toLowerCase().includes(searchTerm.toLowerCase())) {
                 return true;
-            } if ((event.eventName).toLowerCase().includes(searchTerm.toLowerCase())) {
+            }
+            if ((event.eventName).toLowerCase().includes(searchTerm.toLowerCase())) {
                 return true;
             }
 
@@ -61,13 +63,30 @@ export const SearchBar = () => {
 
     return (
         <Paper className={classes.paper}>
-            <TextField
-                variant="outlined"
-                id='outlined-basic'
-                className={classes.search}
-                placeholder="Search for an event"
-                onChange={e => {handleSearchChange(e.target.value)}}
-            />
+            <Grid container>
+                <Grid item xs={3}/>
+                <Grid item xs={6}>
+                    <TextField
+                        fullWidth
+                        variant="outlined"
+                        id='outlined-basic'
+                        className={classes.search}
+                        placeholder="Search for an event"
+                        onChange={e => {
+                            handleSearchChange(e.target.value)
+                        }}
+                    />
+                </Grid>
+                <Grid item xs={3} container justify={"flex-end"}>
+                    <Button variant={"contained"} color={"default"}
+                            onClick={() => setOpen(true)}
+                    >
+                        New
+                    </Button>
+                </Grid>
+                <NewEventDialog open={isOpen} onClose={() => setOpen(false)}/>
+            </Grid>
+
         </Paper>
     );
 }
